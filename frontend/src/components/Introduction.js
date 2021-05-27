7cb1554e97b0bd0693ebbe9654dd54b1b1419c4a
brynsroberts <bryan.roberts.home@gmail.com>
2021-05-26 20:00:08 -0700

    update introduction

--- frontend/src/components/Introduction.js
+++ frontend/src/components/Introduction.js
@@ -8,13 +8,15 @@ function Description() {
       <Card.Body>
         <p>
           Implements a REST API that uses proper resource based URLs, pagination
-          and status codes. Uses Google OAuth 2.0 to create and authorize users
-          for protected resourses. Application is deployed on Google Cloud
-          Platform using Google App Engine and Datastore NoSQL database. All
-          code can be found in the linked GitHub repository along with a Postman
+          and status codes. Uses Google OAuth API as a JWT provider to
+          authenticate users for protected resources. Written using MVC
+          desing-pattern with React on the front-end and Node.js/Express on the
+          back-end. Application is deployed on Google Cloud Platform using
+          Google App Engine and Google Cloud Datastore NoSQL database. All code
+          can be found in the linked GitHub repository along with a Postman
           collection and environemt to test API functionality.
         </p>
-        <h5>REST API Base URL</h5>
+        <h4>REST API Base URL</h4>
         <p>https://robertb2-finalproject.wl.r.appspot.com</p>
       </Card.Body>
     </Card>
